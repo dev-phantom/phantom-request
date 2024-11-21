1f227dc68bb71589035e5feb5d0fa08d6ccd243d
dev-phantom <godsfavour1975@gmail.com>
2024-11-21 03:45:36 +0100

    fix: fixed all major issues

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-export { useAxiosGet as phantomGet } from './src/useAxiosGet';
-export { useAxiosPost as phantomPost } from './src/useAxiosPost';
-export { useAxiosPatch as phantomPatch } from './src/useAxiosPatch';
-export { useAxiosPut as phantomPut } from './src/useAxiosPut';
-export { useAxiosDelete as phantomDelete } from './src/useAxiosDelete';
-export { logout, logoutRedirect } from './src/logout';
+export { phantomGet } from "./src/phantomGet";
+export { phantomPost } from "./src/phantomPost";
+export { phantomPatch } from "./src/phantomPatch";
+export { phantomPut } from "./src/phantomPut";
+export { phantomDelete } from "./src/phantomDelete";
+export { logout, logoutRedirect } from "./src/logout";
